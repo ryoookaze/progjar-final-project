33bfdfc256abb1b14e4c445a363a373106e20c95
Ananda Afryan Gemilang <anandalark007@gmail.com>
2019-05-15 02:09:51 +0700

    Updated with auth(Not finished)

--- client.py
+++ client.py
@@ -1,13 +1,17 @@
 import socket
 import threading
 import sys
+import uuid
 import pickle
 
 IPclient=sys.argv[1]
 class Cliente():
-	"""docstring for Cliente"""
 	def __init__(self, host="localhost", port=4000):
-		
+		self.sessions={}
+		self.users = {}
+		self.users['messi']={ 'nama': 'Lionel Messi', 'negara': 'Argentina', 'password': 'surabaya', 'incoming' : {}, 'outgoing': {}}
+		self.users['henderson']={ 'nama': 'Jordan Henderson', 'negara': 'Inggris', 'password': 'surabaya', 'incoming': {}, 'outgoing': {}}
+		self.users['lineker']={ 'nama': 'Gary Lineker', 'negara': 'Inggris', 'password': 'surabaya','incoming': {}, 'outgoing':{}}
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.sock.connect((str(host), int(port)))
 
@@ -18,23 +22,39 @@ class Cliente():
 
 		while True:
 			msg = input('->')
-			if msg != 'out':
+			if msg != "{quit}":
 				self.send_msg(msg)
 			else:
 				self.sock.close()
 				sys.exit()
+	def progress(self, data):
+		try:
+			command = input('->')
+			if (command == 'auth'):
+				username=j[1].strip()
+				password=j[2].strip()
+				print("auth {}" . format(username))
+				return self.user_authentication(username, password)
 
+	def user_authentication(self, username, password):
+		if (username not in self.users):
+			return { 'status': 'ERROR', 'message': 'User Tidak Ada' }
+ 		if (self.users[username]['password']!= password):
+			return { 'status': 'ERROR', 'message': 'Password Salah' }
+		tokenid = str(uuid.uuid4()) 
+		self.sessions[tokenid]={ 'username': username, 'userdetail':self.users[username]}
+		return { 'status': 'OK', 'tokenid': tokenid }
+		
 	def msg_recv(self):
 		while True:
 			try:
 				data = self.sock.recv(1024)
 				if data:
-					print(pickle.loads(data))
+					print(str(pickle.loads(data)))
 			except:
 				pass
 
 	def send_msg(self, msg):
-		self.sock.send(pickle.dumps(str(IPclient)+" says "+msg))
-
+		self.sock.send(bytes(msg, "utf-8"))
 
 c = Cliente()
